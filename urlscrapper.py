4920f2e1b99d82ed58a6e009d9cdae72047b7a82
innexa <32698252+innexa@users.noreply.github.com>
2019-05-03 23:13:13 +0300

    Update urlscrapper.py
    lxml added

--- urlscrapper.py
+++ urlscrapper.py
@@ -5,7 +5,7 @@ url = "https://target.com/"
 
 page = requests.get(url)    
 data = page.text
-soup = BeautifulSoup(data)
+soup = BeautifulSoup(data, "lxml")
 
 for link in soup.find_all('a'):
     print(link.get('href'))
